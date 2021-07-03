7eb9d89d788b2d4db8ee3b344dfe4224487aaadf
AdamBures <47189693+AdamBures@users.noreply.github.com>
2021-07-03 22:46:31 +0200

    Added constants

---
+++ CONSTANTS.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+X = 50
+Y = 50
+WIDTH_PLAYER = 40
+HEIGHT_PLAYER = 60
+VEL = 5
+
+IMAGE = r"Pictures\images.jpg"
+BACKGROUND = pygame.image.load(IMAGE)
+WIDTH = BACKGROUND.get_width()
+HEIGHT = BACKGROUND.get_height()
+
+FONT_TYPE = 'freesansbold.ttf'
+FONT_SIZE = 24
