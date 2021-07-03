9b2c1036fcc47df914598b555a9010a509501454
AdamBures <47189693+AdamBures@users.noreply.github.com>
2021-07-03 22:46:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import sys
-import pygame
+from CONSTANTS import *
 
 pygame.init()
 
-BACKGROUND = pygame.image.load(r"C:\Users\sasi1\Downloads\images.jpg")
-WIDTH = BACKGROUND.get_width()
-HEIGHT = BACKGROUND.get_height()
 SCREEN = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Survival game")
 SCREEN.blit(BACKGROUND, (0, 0))
-FONT_TYPE = 'freesansbold.ttf'
-FONT_SIZE = 24
+
+
 FONT = pygame.font.Font(FONT_TYPE, FONT_SIZE)
 TEXT = FONT.render("SURVIVAL GAME", False, "WHITE")
 TEXT_RECT = TEXT.get_rect(center=(WIDTH/2, (HEIGHT/2)-100))
