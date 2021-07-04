f6ef605948f4e607f09f1b83899d5aa382fe58d2
AdamBures <47189693+AdamBures@users.noreply.github.com>
2021-07-05 00:20:23 +0200

    Added Buttons and other menus

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import pygame.mouse
 from CONSTANTS import *
 
 pygame.init()
@@ -14,18 +15,51 @@ def draw_text(text, font, color, surface, distance):
     surface.blit(text_obj, text_rect)
 
 
+def start_game():
+    pass
+
+
+def options():
+    pass
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
 def main():
+    button_1 = pygame.Rect(260, 155, 200, 40)
+    button_2 = pygame.Rect(260, 215, 200, 40)
+    button_3 = pygame.Rect(260, 275, 200, 40)
+
+    pygame.draw.rect(SCREEN, (118, 118, 118), button_1)
+    pygame.draw.rect(SCREEN, (118, 118, 118), button_2)
+    pygame.draw.rect(SCREEN, (118, 118, 118), button_3)
     draw_text("START", pygame.font.Font(FONT_TYPE, FONT_SIZE), "WHITE", SCREEN, -30)
     draw_text("OPTIONS", pygame.font.Font(FONT_TYPE, FONT_SIZE), "WHITE", SCREEN, 30)
-    draw_text("EXIT", pygame.font.Font(FONT_TYPE, FONT_SIZE), "WHITE", SCREEN, 80)
+    draw_text("EXIT", pygame.font.Font(FONT_TYPE, FONT_SIZE), "WHITE", SCREEN, 90)
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                terminate()
 
-            pygame.display.update()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+
+                if button_1.collidepoint(mouse_pos):
+                    start_game()
+
+                if button_2.collidepoint(mouse_pos):
+                    options()
+
+                if button_3.collidepoint(mouse_pos):
+                    terminate()
+
+        pygame.display.update()
+        CLOCK.tick(FPS)
 
 
 if __name__ == '__main__':
