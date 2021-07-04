65e99e047b2aaedc7cc7de96251d1bbd9d5bcfb4
AdamBures <47189693+AdamBures@users.noreply.github.com>
2021-07-05 00:21:27 +0200

    Updated constants and fixed the pygame intro

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,5 +1,6 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
-
 
 X = 50
 Y = 50
@@ -7,11 +8,14 @@ WIDTH_PLAYER = 40
 HEIGHT_PLAYER = 60
 VEL = 5
 
+FPS = 60
+CLOCK = pygame.time.Clock()
+
 IMAGE = r"Pictures\images.jpg"
 BACKGROUND = pygame.image.load(IMAGE)
 WIDTH = BACKGROUND.get_width()
 HEIGHT = BACKGROUND.get_height()
 
 
-FONT_TYPE = 'Fonts\pixeboy.ttf'
+FONT_TYPE = r'Fonts\pixeboy.ttf'
 FONT_SIZE = 60
