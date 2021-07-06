08be73ed559e3e0aaa8d7489c224ceb05ad21b42
AdamBures <47189693+AdamBures@users.noreply.github.com>
2021-07-06 22:47:03 +0200

    Added Sprites

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,6 +1,29 @@
+import pygame
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
+
+
+WALK_RIGHT = [pygame.image.load(r'Sprites\R1.png'),
+              pygame.image.load(r'Sprites\R2.png'),
+              pygame.image.load(r'Sprites\R3.png'),
+              pygame.image.load(r'Sprites\R4.png'),
+              pygame.image.load(r'Sprites\R5.png'),
+              pygame.image.load(r'Sprites\R6.png'),
+              pygame.image.load(r'Sprites\R7.png'),
+              pygame.image.load(r'Sprites\R8.png'),
+              pygame.image.load(r'Sprites\R9.png')]
+
+WALK_LEFT = [pygame.image.load(r'Sprites\L1.png'),
+             pygame.image.load(r'Sprites\L2.png'),
+             pygame.image.load(r'Sprites\L3.png'),
+             pygame.image.load(r'Sprites\L4.png'),
+             pygame.image.load(r'Sprites\L5.png'),
+             pygame.image.load(r'Sprites\L6.png'),
+             pygame.image.load(r'Sprites\L7.png'),
+             pygame.image.load(r'Sprites\L8.png'),
+             pygame.image.load(r'Sprites\L9.png')]
+
+PLAYER = pygame.image.load(r'Sprites\standing.png')
 
 X = 50
 Y = 50
@@ -13,6 +36,7 @@ CLOCK = pygame.time.Clock()
 
 IMAGE = r"Pictures\images.jpg"
 BACKGROUND = pygame.image.load(IMAGE)
+LEVEL1 = pygame.image.load(r"Pictures\level1.jpg")
 WIDTH = BACKGROUND.get_width()
 HEIGHT = BACKGROUND.get_height()
 
